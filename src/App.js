64361c2eb8d5951c13dafe345fa4696adda1afb6
manduul <manduul@LEAP-03s-iMac.local>
2021-06-15 11:58:23 +0800

    qr

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+// import Shop from './components/Shop'
+
+const BaseUrl = 'https://us-central1-qr-payment-c672a.cloudfunctions.net'
 
 function App() {
   const [ qrImg, setQrImg ] = useState();
+  const [ isPaid, setIsPaid ] = useState(false);
 
   const onButtonClicked = async () => {
-    const { qr, invoiceId } = await axios.post('https://us-central1-qr-payment-demo-6a1fe.cloudfunctions.net/create_invoice', {
+    const response = await axios.post(`${ BaseUrl }/create_invoice`, {
       price: 5000,
-    }).data
+    })
+    console.log(response);
+    const { qr, invoiceId } = response.data;
 
     setQrImg(qr)
 
-    firestore.doc(`invoice/${ invoiceId }`).onSnapshot((doc) => {
-      console.log(doc);
+    firestore.doc(`invoices/${ invoiceId }`).onSnapshot((doc) => {
+      if (doc.data().status === 'paid') {
+        setIsPaid(true)
+      }
     })
     
   }
@@ -23,7 +31,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={ qrImg ? qrImg : logo} className="App-logo" alt="logo" />
+        {
+          isPaid ? (
+            <h2>Order successfully paid!</h2> 
+          ) : (
+            <img src={ qrImg ? qrImg : logo} className="App-logo" alt="logo" />
+          )
+        }
+      
         <button onClick={ onButtonClicked }>Payment</button>
       </header>
     </div>
