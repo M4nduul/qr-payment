301e806b62e54e09c95c5c8fc1684e5474f7a5dc
manduul <manduul@LEAP-03s-iMac.local>
2021-06-16 11:59:23 +0800

    err: bankInvoiceId

--- functions/index.js
+++ functions/index.js
@@ -8,36 +8,75 @@ admin.initializeApp()
 
 const BaseUrl = 'https://us-central1-qr-payment-c672a.cloudfunctions.net'
 
-exports.create_invoice = functions.https.onRequest(async (request, response) => {
+exports.createInvoice = functions.https.onRequest(async (request, response) => {
     cors(request, response, async () => {
         const db = admin.firestore()
         const invoice = await db.collection(`invoices`).add({
-            name: 'some name',
+            products: request.body.products,
+            amount: request.body.amount,
         })
 
-        // qr axios postoor uusgeh
-        qrcode.toDataURL(`${ BaseUrl }/qr_generate?invoiceId=${ invoice.id }/`, (err, code) => {
-            if(err) return console.log('error occurred')
+        const invoiceRequest = await axios.post(`${ BaseUrl }/qrRequestToBank`, {
+            invoiceId: invoice.id,
+            amount: request.body.amount,
+            shopUrl: `${ BaseUrl }/shopPaymentReceived?invoiceId=${ invoice.id }/`,
+          })      
 
-            response.send({ 
-                qr: code,
-                invoiceId: invoice.id,
-            })
+        const { qr } = invoiceRequest.data
+            
+        response.send({ 
+            qr,
+            invoiceId: invoice.id,
+            amount: request.body.amount,
         })
     })
-    
 });
 
-exports.qr_generate = functions.https.onRequest(async (request, response) => {
+exports.qrRequestToBank = functions.https.onRequest(async (request, response) => {
+    cors(request, response, async () => {
+        const db = admin.firestore()
+        const bankInvoice = await db.collection(`bank_invoices`).add({
+            shopInvoiceId: request.body.invoiceId,
+            amount: request.body.amount,
+            shopUrl: request.body.shopUrl
+        })
+
+        qrcode.toDataURL(`${ BaseUrl }/qrPaidBank?bankInvoiceId=${ bankInvoice.id }/`, (err, code) => {
+            if(err) return console.log('error occurred')
+            
+            response.send({ 
+                qr: code,
+                bankInvoiceId: bankInvoice.id,
+                amount: request.body.amount
+            })
+        })
+
+    })
+})
+
+exports.qrPaidBank = functions.https.onRequest(async (request, response) => {       
+    const db = admin.firestore()
+    const { bankInvoiceId } = request.query
+
+    await db.doc(`bank_invoices/${ bankInvoiceId }`).set({
+        status: 'paid',
+    },{
+        merge: true,
+    })
+    
+    const callbackUrl = await db.collection(`bank_invoices/${ bankInvoiceId}`).get()
+    await axios.post(callbackUrl.data().shopUrl)
+    
+}) 
+
+exports.shopPaymentReceived = functions.https.onRequest(async (request, response) => {       
     const db = admin.firestore()
     const { invoiceId } = request.query
 
     await db.doc(`invoices/${ invoiceId }`).set({
         status: 'paid',
-
     },{
         merge: true,
     })
     
 }) 
-
--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import { firestore } from './firebase'
 import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-// import Shop from './components/Shop'
+import { useEffect, useState } from 'react';
 
 const BaseUrl = 'https://us-central1-qr-payment-c672a.cloudfunctions.net'
 
 function App() {
-  const [ qrImg, setQrImg ] = useState();
-  const [ isPaid, setIsPaid ] = useState(false);
+  const [ qrImg, setQrImg ] = useState()
+  const [ isPaid, setIsPaid ] = useState(false)
+  const [ totalAmount, setTotalAmount ] = useState(5000)
+  const [ products, setProducts ] = useState({apple: 1000, milk: 4000,})
 
+
+  
   const onButtonClicked = async () => {
-    const response = await axios.post(`${ BaseUrl }/create_invoice`, {
-      price: 5000,
+
+    const response = await axios.post(`${ BaseUrl }/createInvoice`, {
+      products: products,
+      amount: totalAmount,
     })
+
     console.log(response);
     const { qr, invoiceId } = response.data;
 
@@ -38,7 +44,6 @@ function App() {
             <img src={ qrImg ? qrImg : logo} className="App-logo" alt="logo" />
           )
         }
-      
         <button onClick={ onButtonClicked }>Payment</button>
       </header>
     </div>
